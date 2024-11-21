ab0f621911991c9724b2a039dad436cd65d674a1
LuckyDg <sadgdev@gmail.com>
2024-11-21 16:06:29 -0500

    add layout UI signUp screen

---
+++ lib/modules/auth/signup/components/form_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final IconData prefixIcon;
+  final TextInputType? inputType;
+  final bool isPassword;
+  final TextEditingController? controller;
+
+  const CustomFormField({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.prefixIcon,
+    this.inputType,
+    this.isPassword = false,
+    this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: inputType,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          labelText: label,
+          hintText: hintText,
+          prefixIcon: Icon(prefixIcon),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.orange),
+          ),
+        ),
+      ),
+    );
+  }
+}
