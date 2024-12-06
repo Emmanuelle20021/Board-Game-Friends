59b9f206f1191ef91c4137efb25bb496641ab9c0
Emma Mora <emmanu20021@gmail.com>
2024-12-06 09:41:35 -0600

    chore: Refactor UserService class to use userRef instead of _userRef

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -2,9 +2,9 @@ import 'package:board_game_friends/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserService {
-  UserService._();
+  UserService();
 
-  static final CollectionReference<User> _userRef =
+  static CollectionReference<User> userRef =
       FirebaseFirestore.instance.collection('Users').withConverter(
           fromFirestore: (snapshots, __) => User.fromMap(
                 data: snapshots.data()!,
@@ -14,7 +14,7 @@ class UserService {
 
   static Future<void> createUser(User user) async {
     try {
-      await _userRef.doc().set(user);
+      await userRef.doc(user.uid).set(user);
     } catch (e) {
       rethrow;
     }
@@ -22,7 +22,7 @@ class UserService {
 
   static Future<void> updateUser(User user) async {
     try {
-      await _userRef.doc(user.uid).set(user);
+      await userRef.doc(user.uid).set(user);
     } catch (e) {
       rethrow;
     }
@@ -30,35 +30,39 @@ class UserService {
 
   static Future<void> deleteUser(User user) async {
     try {
-      await _userRef.doc(user.uid).delete();
+      await userRef.doc(user.uid).delete();
     } catch (e) {
       rethrow;
     }
   }
 
-  static Stream<List<User>> getUsers() {
-    return _userRef.snapshots().map(
+  static Stream<List<User?>> getUsers() {
+    return userRef.snapshots().map(
           (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
         );
   }
 
-  static Stream<User> getUser(String uid) {
-    return _userRef.doc(uid).snapshots().map((snapshot) => snapshot.data()!);
+  static Stream<User?> getUser(String uid) {
+    return userRef.doc(uid).snapshots().map((snapshot) => snapshot.data()!);
   }
 
-  static Future<User> getUserById(String uid) async {
+  static Future<User?> getUserById(String uid) async {
     try {
-      final user = await _userRef.doc(uid).get();
+      final user = await userRef.doc(uid).get();
       return user.data()!;
     } catch (e) {
       rethrow;
     }
   }
 
-  static Future<User> getUserByEmail(String email) async {
+  static Future<User?> getUserByEmail(String email) async {
     try {
-      final user = await _userRef.where('email', isEqualTo: email).get();
-      return user.docs.first.data();
+      final querySnapshot =
+          await userRef.where('email', isEqualTo: email).get();
+      if (querySnapshot.docs.isEmpty) {
+        throw Exception('User not found');
+      }
+      return querySnapshot.docs.first.data();
     } catch (e) {
       rethrow;
     }
